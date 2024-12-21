0de1c86430ae913b8b38e2fc9df275a680c99360
BrivaHamisi <brivahamisi@gmail.com>
2024-12-21 21:53:17 +0300

    Added an auth provider Class

---
+++ lib/providers/auth_providers.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum AuthStatus {
+  NotAuthenticated,
+  Authenticating,
+  Authenticated,
+  UserNootFound,
+  Error,
+}
+
+class AuthProvider extends ChangeNotifier {
+  User? user;
+  late AuthStatus status;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  static AuthProvider instance = AuthProvider();
+
+  AuthProvider() {
+    _auth = FirebaseAuth.instance;
+    status = AuthStatus.NotAuthenticated;
+  }
+
+  void loginUserWithEmailandPassword(String _email, String _password) async {
+    status = AuthStatus.Authenticating;
+    notifyListeners();
+    try {
+      UserCredential _result = await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+
+      user = _result.user;
+      status = AuthStatus.Authenticated;
+      notifyListeners();
+
+      //Navigate yo Homepage
+      print("Logged In Successfully");
+    } catch (e) {
+      status = AuthStatus.Error;
+      notifyListeners();
+    }
+  }
+}
